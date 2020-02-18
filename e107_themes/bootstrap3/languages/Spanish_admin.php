1413439ab46ef43851fb4c8fb1de5cf9e8fedbe4
Nuri Ismail Onbashi <nuri.bg@live.com>
2020-02-18 03:07:58 +0100

    Update Spanish_admin.php

--- e107_themes/bootstrap3/languages/Spanish_admin.php
+++ e107_themes/bootstrap3/languages/Spanish_admin.php
@@ -4,23 +4,23 @@
 |        e107 website content management system Spanish Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2016/04/12 18:33:10
+|        Last Modified: 2020/02/17 20:35:00
 |
 |        $Author: Norwayman $
 +---------------------------------------------------------------+
 */
 
 define("LAN_THEMEPREF_00", "Marca:");
-define("LAN_THEMEPREF_01", "Barra de navegación Alineación:");
-define("LAN_THEMEPREF_02", "Colocación de Registro/Inicio de sesión:");
+define("LAN_THEMEPREF_01", "Alineación de la barra de navegación::");
+define("LAN_THEMEPREF_02", "Ubicación de registro/inicio de sesión:");
 define("LAN_THEMEPREF_03", "Estilos Bootswatch:");
 define("LAN_THEMEPREF_04", "Nombre del Sitio");
 define("LAN_THEMEPREF_05", "Logotipo");
-define("LAN_THEMEPREF_06", "Logotipo &amp; Nombre del Sitio");
-define("LAN_THEMEPREF_07", "Izquierda");
-define("LAN_THEMEPREF_08", "Derecha");
-define("LAN_THEMEPREF_09", "Superior");
-define("LAN_THEMEPREF_10", "Fondo");
+define("LAN_THEMEPREF_06", "Logotipo y nombre del sitio");
+define("LAN_THEMEPREF_07", "izquierda");
+define("LAN_THEMEPREF_08", "derecha");
+define("LAN_THEMEPREF_09", "superior");
+define("LAN_THEMEPREF_10", "inferior");
 
 
 ?>
