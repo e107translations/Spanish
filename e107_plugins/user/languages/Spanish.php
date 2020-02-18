e725d134f83357f94083c012fc192ce57ea13585
Nuri Ismail Onbashi <nuri.bg@live.com>
2020-02-18 03:33:24 +0100

    Update Spanish.php

--- e107_plugins/user/languages/Spanish.php
+++ e107_plugins/user/languages/Spanish.php
@@ -1,18 +1,24 @@
 <?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Spanish Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/02/17 20:37:12
+|
+|        $Author: 1nn7erpLaY $
++---------------------------------------------------------------+
+*/
 
-// Bing-Translated Language file 
-// Generated for e107 v2.x by the Multi-Language Plugin
-// https://github.com/e107inc/multilan
-
-define("UTHEME_MENU_L1", "Conjunto");
+define("UTHEME_MENU_L1", "Establecer");
 define("UTHEME_MENU_L2", "Seleccionar idioma");
 define("UTHEME_MENU_L3", "tablas");
-define("LAN_UMENU_THEME_1", "Tema conjunto");
-define("LAN_UMENU_THEME_2", "Seleccionar tema");
+define("LAN_UMENU_THEME_1", "Establecer diseño");
+define("LAN_UMENU_THEME_2", "Seleccionar diseño");
 define("LAN_UMENU_THEME_3", "usuarios:");
-define("LAN_UMENU_THEME_4", "Permitir que los temas que los usuarios pueden seleccionar");
-define("LAN_UMENU_THEME_5", "Actualización");
-define("LAN_UMENU_THEME_6", "Temas disponibles para los usuarios");
-define("LAN_UMENU_THEME_7", "Clase que puede seleccionar temas");
-define("LAN_UMENU_THEME_8", "Temas permitidos:");
-define("LAN_UMENU_THEME_9", "Clase que puede seleccionar los temas:");
+define("LAN_UMENU_THEME_4", "Habilitar los diseños que los usuarios pueden seleccionar");
+define("LAN_UMENU_THEME_5", "Actualizar");
+define("LAN_UMENU_THEME_6", "Diseños disponibles para los usuarios");
+define("LAN_UMENU_THEME_7", "Clase que puede seleccionar diseños");
+define("LAN_UMENU_THEME_8", "Diseños permitidos:");
+define("LAN_UMENU_THEME_9", "Clase que puede seleccionar diseños:");
