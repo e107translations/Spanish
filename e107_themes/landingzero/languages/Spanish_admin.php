07d6a87dc003f44e1b8b4f80de83dd47e71c2d76
Nuri Ismail Onbashi <nuri.bg@live.com>
2020-02-18 02:46:53 +0100

    Update Spanish_admin.php

--- e107_themes/landingzero/languages/Spanish_admin.php
+++ e107_themes/landingzero/languages/Spanish_admin.php
@@ -4,16 +4,16 @@
 |        e107 website content management system Spanish Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/11/11 21:48:03
+|        Last Modified: 2020/02/17 20:14:54
 |
-|        $Author: Norwayman $
+|        $Author: 1nn7erpLaY $
 +---------------------------------------------------------------+
 */
 
-define("LAN_LZ_THEMEPREF_00", "Fondo de imagen para video [1920x1080px]");
-define("LAN_LZ_THEMEPREF_01", "Fondo de imagen para dispositivos móviles");
-define("LAN_LZ_THEMEPREF_02", "Primer cuadro de video [1920x1080px]");
+define("LAN_LZ_THEMEPREF_00", "Imagen de fondo para video [1920x1080px]");
+define("LAN_LZ_THEMEPREF_01", "Imagen de fondo para dispositivos móviles");
+define("LAN_LZ_THEMEPREF_02", "Portada para video [1920x1080px]");
 define("LAN_LZ_THEMEPREF_03", "Ruta URL al video del encabezado en formato mp4");
-define("LAN_LZ_THEMEPREF_04", "Ubicación de registro / inicio de sesión");
+define("LAN_LZ_THEMEPREF_04", "Ubicación de registro/inicio de sesión");
 define("LAN_LZ_THEMEPREF_05", "superior");
-define("LAN_LZ_THEMEPREF_06", "fondo");
+define("LAN_LZ_THEMEPREF_06", "inferior");
