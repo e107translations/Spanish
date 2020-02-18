ea7900853eccc5d2d47253ea62e5f811105d4616
Nuri Ismail Onbashi <nuri.bg@live.com>
2020-02-18 02:59:03 +0100

    Update Spanish.php

--- e107_themes/landingzero/languages/Spanish.php
+++ e107_themes/landingzero/languages/Spanish.php
@@ -4,29 +4,29 @@
 |        e107 website content management system Spanish Language File
 |        Released under the terms and conditions of the
 |        GNU General Public License (http://gnu.org).
-|        Last Modified: 2017/11/11 21:44:34
+|        Last Modified: 2020/02/17 20:32:12
 |
-|        $Author: Norwayman $
+|        $Author: 1nn7erpLaY $
 +---------------------------------------------------------------+
 */
 
 define("LAN_LZ_THEME_00", "OK");
-define("LAN_LZ_THEME_01", "Comience");
-define("LAN_LZ_THEME_02", "Conmutar video");
-define("LAN_LZ_THEME_03", "Su navegador no soporta la etiqueta de vídeo. Te sugiero que actualices tu navegador.");
+define("LAN_LZ_THEME_01", "Comenzar");
+define("LAN_LZ_THEME_02", "Alternar vídeo");
+define("LAN_LZ_THEME_03", "Este navegador no soporta la etiqueta <video>. Actualiza tu navegador.");
 define("LAN_LZ_THEME_04", "Diseños flexibles");
 define("LAN_LZ_THEME_05", "Cerrar");
-define("LAN_LZ_THEME_06", "Caracteristicas");
-define("LAN_LZ_THEME_07", "Por favor ingrese un mensaje.");
+define("LAN_LZ_THEME_06", "Funciones");
+define("LAN_LZ_THEME_07", "Por favor introduce un mensaje.");
 define("LAN_LZ_THEME_08", "Número de teléfono");
-define("LAN_LZ_THEME_09", "Por favor, introduzca su número de teléfono.");
+define("LAN_LZ_THEME_09", "Por favor, introduce tu número de teléfono.");
 define("LAN_LZ_THEME_10", "Seguir");
 define("LAN_LZ_THEME_11", "Teléfono");
-define("LAN_LZ_THEME_12", "Su mensaje aquí...
+define("LAN_LZ_THEME_12", "Aquí tu mensaje...
 ");
-define("LAN_LZ_THEME_13", "Estar en contacto");
-define("LAN_LZ_THEME_14", "Nos encantan los comentarios. Rellene el siguiente formulario y nos pondremos en contacto con usted lo antes posible.");
-define("LAN_LZ_THEME_15", "Cuéntanos tu correo electrónico");
-define("LAN_LZ_THEME_16", "Suscríbete para recibir actualizaciones");
-define("LAN_LZ_THEME_17", "Manténgase enterardo");
-define("LAN_LZ_THEME_18", "TAMBIÉN PODRÍA GUSTARTE");
+define("LAN_LZ_THEME_13", "Ponte en contacto");
+define("LAN_LZ_THEME_14", "Nos encantan los comentarios. Completa el siguiente formulario y nos pondremos en contacto contigo lo antes posible.");
+define("LAN_LZ_THEME_15", "Dinos tu correo electrónico");
+define("LAN_LZ_THEME_16", "Suscríbete para recibir futuras actualizaciones");
+define("LAN_LZ_THEME_17", "Mantente informado");
+define("LAN_LZ_THEME_18", "TAMBIÉN TE PODRÍA GUSTAR");
