864a7a218df1e746a42eb2c37757091e2be59697
leo <the_guardian2@hotmail.com>
2020-12-31 12:53:20 +0100

    Add files via upload

---
+++ e107_themes/_blank/languages/Spanish.php
@@ -0,0 +1,13 @@
+<?php
+define("EMESSLAN_TITLE_INFO", "Información del sistema");
+//define("EMESSLAN_TITLE_ERROR", "Error");//new > LAN_ERROR
+define("EMESSLAN_TITLE_SUCCESS", "Éxito");
+define("EMESSLAN_TITLE_WARNING", "Advertencia");
+define("EMESSLAN_TITLE_DEBUG", "Depuración del sistema");
+
+define("LAN_THEME_1", "Los comentarios de este artículo están desactivados");
+define("LAN_THEME_2", "Leer/Lugar Comentario: ");
+define("LAN_THEME_3", "Leer más......");
+// define("LAN_THEME_4", "Trackbacks: ");
+define("LAN_THEME_5", "Publicado por ");
+define("LAN_THEME_6", "en ");
