6d27be1e47dae9a02e96b4fe18946ef962a6dfeb
leo <the_guardian2@hotmail.com>
2021-02-05 21:34:16 +0100

    05-02-2021

--- e107_languages/Spanish/admin/help/wmessage.php
+++ e107_languages/Spanish/admin/help/wmessage.php
@@ -16,5 +16,5 @@
 
 if (!defined('e107_INIT')) { exit; }
 
-$text = "This page allows you to set a message that will appear at the top of your front page all the time it's activated. You can set a different message for guests, registered/logged-in members and administrators.";
-$ns -> tablerender("WMessage Help", $text);
+$text = defset('WMLAN_13');
+$ns -> tablerender( defset('WMLAN_12'), $text);
