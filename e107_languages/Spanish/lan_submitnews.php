35cb3475187af6c223dc4770c549414ef32a9dc1
leo <the_guardian2@hotmail.com>
2021-02-05 21:56:40 +0100

    05-02-2021

--- e107_languages/Spanish/lan_submitnews.php
+++ e107_languages/Spanish/lan_submitnews.php
@@ -9,15 +9,18 @@
 |        $Author: Norwayman $
 +---------------------------------------------------------------+
 */
+if(!defined('PAGE_NAME'))
+{
+	define("PAGE_NAME", "Enviar noticias");
+}
 
-define("PAGE_NAME", "Enviar noticias");
-define("LAN_7", "Nombre:");
-define("LAN_112", "Dirección de correo electrónico:");
-define("LAN_133", "Gracias");
+//define("LAN_7", "Nombre:");
+//define("LAN_112", "Dirección de correo electrónico:");
+//define("LAN_133", "Gracias");
 define("LAN_134", "Tu artículo ha sido enviado para su revisión por uno de los administradores del sitio.");
 define("LAN_135", "Noticia:");
 define("LAN_136", "Enviar noticia");
-define("NWSLAN_6", "Categoría");
+//define("NWSLAN_6", "Categoría");
 define("NWSLAN_10", "No hay categorías de noticias");
 define("NWSLAN_11", "Usted no tiene acceso a esta área o que actualmente no está en el sistema.");
 define("NWSLAN_12", "Acceso denegado.");
@@ -30,7 +33,7 @@ define("SUBNEWSLAN_6", "(jpg, gif o png)");
 define("SUBNEWSLAN_7", "Usted debe dar su nombre y correo electrónico");
 define("SUBNEWSLAN_8", "Error cargar imagen");
 define("SUBNEWSLAN_9", "Palabras clave");
-define("SUBNEWSLAN_10", "Resumen");
+//define("SUBNEWSLAN_10", "Resumen");
 define("SUBNEWSLAN_11", "Descripción de Meta");
 define("SUBNEWSLAN_12", "Utilizado por Facebook, etc.");
 define("SUBNEWSLAN_13", "URL de los medios");
