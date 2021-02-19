f9b5aff51ffb0137b95c10523f05665c19126548
leo <the_guardian2@hotmail.com>
2021-02-19 18:36:15 +0100

    Add files via upload
    missing

---
+++ e107_plugins/news/languages/Spanish_global.php
@@ -0,0 +1,16 @@
+<?php
+/*
++ ----------------------------------------------------------------------------+
+|     e107 website system - Dutch Language File.
+|
+|     $URL:$
+|     $Revision:$
+|     $Date:$
+|     $Author: Yvalni, Tgtje $
++----------------------------------------------------------------------------+
+*/
+
+<?php
+
+
+define("LAN_PLUGIN_NEWS_NAME", "Noticias");
