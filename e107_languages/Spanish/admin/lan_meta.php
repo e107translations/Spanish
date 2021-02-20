d40362f106e04957a4a89fb354cc29052208a391
leo <the_guardian2@hotmail.com>
2021-02-20 13:48:45 +0100

    Update lan_meta.php

--- e107_languages/Spanish/admin/lan_meta.php
+++ e107_languages/Spanish/admin/lan_meta.php
@@ -18,4 +18,4 @@ define("METLAN_3", "Uso noticias título y resumen como la descripción meta en 
 define("METLAN_4", "Etiquetas personalizadas (dentro de las etiquetas [x])");
 define("METLAN_5", "Etiquetas personalizadas (después de [x])");
 define("METLAN_6", "Etiquetas personalizadas (antes de [x])");
-define("METLAN_7", "Todos los metadatos o etiquetas HTML personalizadas que se introduzcan aquí (como las etiquetas $lt;script$gt;o el código de Google Analytics) se incluirán en todas las páginas del sitio web en las áreas designadas");
+define("METLAN_7", "Todos los metadatos o etiquetas HTML personalizadas que se introduzcan aquí (como las etiquetas <script> o el código de Google Analytics) se incluirán en todas las páginas del sitio web en las áreas designadas");
