bbb14410d840d6d8086f9bd9537d4f6009645c15
leo <the_guardian2@hotmail.com>
2021-03-18 14:55:39 +0100

    Add files via upload

---
+++ e107_plugins/hero/languages/Spanish/Spanish_admin.php
@@ -0,0 +1,27 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system 
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/12/18 12:42:44
+|
+|        $Author:  $
++----------------------------------------------------------------------+
+*/
+
+define("LAN_HERO_ADMIN_001", "Diapositivas estándar importadas");
+define("LAN_HERO_ADMIN_002", "Falló la importación de diapositivas estándar");
+define("LAN_HERO_ADMIN_003", "Fondo");
+define("LAN_HERO_ADMIN_004", "Imagen/Video");
+define("LAN_HERO_ADMIN_005", "Media");
+define("LAN_HERO_ADMIN_006", "Etiqueta");
+define("LAN_HERO_ADMIN_007", "Asegúrate de habilitar el menú héroe en tu página de inicio utilizando el [Gestor de menús]");
+define("LAN_HERO_ADMIN_009", "Paquete de iconos");
+define("LAN_HERO_ADMIN_010", "Estilo de icono");
+define("LAN_HERO_ADMIN_011", "Intervalo de deslizamiento automatizado");
+define("LAN_HERO_ADMIN_012", "Texto");
+define("LAN_HERO_ADMIN_013", "Animación");
+define("LAN_HERO_ADMIN_014", "Retraso (seg)");
+define("LAN_HERO_ADMIN_015", "Botón");
+define("LAN_HERO_ADMIN_016", "[x] segundo(s)");
---
+++ e107_plugins/hero/languages/Spanish/Spanish_global.php
@@ -0,0 +1,15 @@
+<?php
+/*
++----------------------------------------------------------------------+
+|        e107 website content management system 
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2020/12/18 12:42:44
+|
+|        $Author:  $
++----------------------------------------------------------------------+
+*/
+
+define("LAN_PLUGIN_HERO_NAME", "Hero");
+define("LAN_PLUGIN_HERO_SUMMARY", "Gestión del área 'Hero' de la página de inicio");
+define("LAN_PLUGIN_HERO_DESCRIPTION", "Un deslizador de imagen y texto con viñetas animadas para el área de héroe de su página de inicio");
