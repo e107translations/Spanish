54b6cdc63dfbf175108d730ed6b20b402c81af31
leo <the_guardian2@hotmail.com>
2021-12-04 14:00:42 +0100

    Update Spanish.php

--- e107_plugins/newsletter/languages/Spanish.php
+++ e107_plugins/newsletter/languages/Spanish.php
@@ -11,8 +11,8 @@
 */
 
 define("NLLAN_MENU_CAPTION", "Boletín de noticias");
-//define("NLLAN_48", "Usted está suscrito a este newsletter - si desea darse de baja por favor, haga clic en el botón de abajo.");
-//define("NLLAN_49", "¿Está seguro de que desea que las Naciones Unidas-suscribirse a este newsletter?");
+define("NLLAN_48", "Está suscrito a este boletín. Si desea cancelar su suscripción, haga clic en el botón siguiente.");
+define("NLLAN_49", "¿Está seguro de que quiere darse de baja de este boletín?");
 define("NLLAN_50", "Haga clic en el botón para suscribirse (tu dirección de suscripción es");
 define("NLLAN_51", "Darse de baja");
 define("NLLAN_52", "Suscribirse");
